eb6c65dffd7dcd9c84bb92fd5afcd5ed66bdaea5
boikot5225-afk <boikot5225@gmail.com>
2026-06-23 22:19:48 +0300

    feat: add language toolbar

--- js/lang-separation.js
+++ js/lang-separation.js
@@ -1,3 +1,14 @@
 const AN2_LANGS = { fr: 'French', zh: 'Chinese' };
 function activeLanguage() { return 'fr'; }
-console.log('x');
+function renderBar() {
+  const nav = document.querySelector('nav');
+  if (!nav) return;
+  let bar = document.getElementById('an2-langbar');
+  if (!bar) {
+    bar = document.createElement('div');
+    bar.id = 'an2-langbar';
+    nav.insertAdjacentElement('afterend', bar);
+  }
+  bar.innerHTML = '<button>French</button><button>Chinese</button>';
+}
+renderBar();
