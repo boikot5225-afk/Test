f9a2b6ae1009afbe7893e2d69c6e50677ab03ff4
boikot5225-afk <boikot5225@gmail.com>
2026-07-16 19:46:29 +0300

    Activate reliable S2 proof module

--- js/reader/chapter-render.js
+++ js/reader/chapter-render.js
@@ -1,2 +1,2 @@
-import './semantic-stage1-proof.js?v=1';
-export { createReaderChapterRenderer } from './chapter-render-stage1.js?v=2';
+import './semantic-stage1-proof-v2.js?v=1';
+export { createReaderChapterRenderer } from './chapter-render-stage1.js?v=3';
