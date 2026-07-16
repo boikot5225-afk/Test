e622dfa13ce5b59754a8a861d43b06d46af0da4d
boikot5225-afk <boikot5225@gmail.com>
2026-07-16 20:07:05 +0300

    Remove temporary S2 proof hook from reader runtime

--- js/reader/chapter-render.js
+++ js/reader/chapter-render.js
@@ -1,2 +1,1 @@
-import './semantic-stage1-proof-v2.js?v=1';
-export { createReaderChapterRenderer } from './chapter-render-stage1.js?v=3';
+export { createReaderChapterRenderer } from './chapter-render-stage1.js?v=4';
