c59917990c452c1c0bc747b9354cc433766036e6
boikot5225-afk <boikot5225@gmail.com>
2026-07-18 13:05:19 +0300

    Load footnote-enabled stage1 renderer

--- js/reader/chapter-render-dialogue.js
+++ js/reader/chapter-render-dialogue.js
@@ -1,4 +1,4 @@
-import { createReaderChapterRenderer as createStage1Renderer } from './chapter-render-stage1.js?v=5';
+import { createReaderChapterRenderer as createStage1Renderer } from './chapter-render-stage1.js?v=6';
 import {
   normalizeSemanticBookLineItems,
   normalizeSemanticBookTextChunks,
