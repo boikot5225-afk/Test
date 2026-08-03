86ee17ec2bac9d95876d20d0393b950a2dddb379
boikot5225-afk <boikot5225@gmail.com>
2026-08-03 09:06:28 +0300

    test(smartbook): include book-language gate in smoke run

--- tools/smartbook-chinese-pinyin/scripts/test.sh
+++ tools/smartbook-chinese-pinyin/scripts/test.sh
@@ -9,6 +9,7 @@ kotlinc \
   "$ROOT/src/main/kotlin/smartbook/pinyin/ChineseSegmenter.kt" \
   "$ROOT/src/main/kotlin/smartbook/pinyin/ContextualPinyinResolver.kt" \
   "$ROOT/src/main/kotlin/smartbook/pinyin/PinyinPlanner.kt" \
+  "$ROOT/src/main/kotlin/smartbook/pinyin/BookLanguage.kt" \
   "$ROOT/src/test/kotlin/smartbook/pinyin/SegmenterSpec.kt" \
   -include-runtime -d "$BUILD/smartbook-pinyin-tests.jar"
 java -jar "$BUILD/smartbook-pinyin-tests.jar"
