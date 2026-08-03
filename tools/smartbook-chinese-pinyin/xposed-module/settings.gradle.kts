c05cea2cf8531b7d9903b84b4986c3790dce0313
boikot5225-afk <boikot5225@gmail.com>
2026-08-03 08:39:30 +0300

    build(smartbook): initialise pinyin Xposed module

---
+++ tools/smartbook-chinese-pinyin/xposed-module/settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://raw.githubusercontent.com/rovo89/XposedBridge/gh-pages/")
+    }
+}
+rootProject.name = "smartbook-pinyin-xposed"
